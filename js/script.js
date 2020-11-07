2b27275c837a2fbd90fc6261c0502de5a3724ff7
Lauren Plenger <laurenb08@gmail.com>
2020-11-07 13:44:36 -0800

    added comment to js so we can reference correct format for API queries

--- js/script.js
+++ js/script.js
@@ -8,3 +8,5 @@ console.log(searchTerm);
 console.log(numRecords);
 console.log(startYear);
 console.log(endYear);
+
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "begin_date" + startYear + "0101" + endYear + "1231" + "&api-key=" apiKey;
