a8a5a9eb8b75cf4291ce872c12b519eca2a45abf
Lauren Plenger <laurenb08@gmail.com>
2020-11-07 13:25:29 -0800

    added variables for User Input fields

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-var apiKey = JAoPApWNBaQdmRUeFlleD8HVhPkZlGNA;
+var apiKey = "JAoPApWNBaQdmRUeFlleD8HVhPkZlGNA";
+var searchTerm = $("#searchTerm").val();
+var numRecords = $("#numRecords").val();
+var startYear = $("#startYear").val();
+var endYear = $("#endYear").val();
+
+console.log(searchTerm);
+console.log(numRecords);
+console.log(startYear);
+console.log(endYear);
